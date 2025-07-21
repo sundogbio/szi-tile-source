2495a96b133f4e8ef352b8f210e2644c643354f8
Richard Tunnicliffe <rdtunnicliffe@gmail.com>
2025-07-21 09:45:56 +0100

    Renamed (un)compressed folders to (un)zipped
    The names were misleading, as the contents of both folders are compressed images,
    the difference being that one has those images zipped up (without further
    compression), and one doesn't.

--- src/main.test.js
+++ src/main.test.js
@@ -9,18 +9,18 @@ import OpenSeadragon from 'openseadragon';
 // that you have done 'npx vite' to serve up the examples on a local server to
 // to start with
 test('Test contents', { timeout: 300_000 }, async () => {
-  const contents = await getContentsOfSziFile('http://localhost:5173/examples/compressed/mixmas.szi');
+  const contents = await getContentsOfSziFile('http://localhost:5173/examples/zipped/mixmas.szi');
   expect(contents.size).toBe(150);
 });
 
 test('Test url mapper', () => {
-  const mapper = new UrlMapper('http://localhost:5173/examples/compressed/mixmas.szi');
+  const mapper = new UrlMapper('http://localhost:5173/examples/zipped/mixmas.szi');
   expect(mapper.sziFilenameWithoutSuffix).toEqual('mixmas');
-  expect(mapper.sziPathWithoutFilename).toEqual('/examples/compressed/');
-  expect(mapper.pathInSziFromDziUrl('http://localhost:5173/examples/compressed/mixmas/mixmas_files/0_0.jpg')).toEqual(
+  expect(mapper.sziPathWithoutFilename).toEqual('/examples/zipped/');
+  expect(mapper.pathInSziFromDziUrl('http://localhost:5173/examples/zipped/mixmas/mixmas_files/0_0.jpg')).toEqual(
     'mixmas/mixmas_files/0_0.jpg',
   );
-  expect(mapper.dziXmlUrl()).toEqual('http://localhost:5173/examples/compressed/mixmas/mixmas.dzi');
+  expect(mapper.dziXmlUrl()).toEqual('http://localhost:5173/examples/zipped/mixmas/mixmas.dzi');
 });
 
 // Again, another "test" that is mostly here so I can happily debug the mechanics of constructing the dervied clas
@@ -28,13 +28,13 @@ test('Test construction', { timeout: 300_000 }, async () => {
   enableSziTileSource(OpenSeadragon);
 
   const sziTileSource = await OpenSeadragon.SziTileSource.createSziTileSource(
-    'http://localhost:5173/examples/compressed/mixmas.szi',
+    'http://localhost:5173/examples/zipped/mixmas.szi',
   );
 
   expect(sziTileSource.getTileUrl(0, 0, 0)).toEqual(
-    'http://localhost:5173/examples/compressed/mixmas/mixmas_files/0/0_0.jpeg',
+    'http://localhost:5173/examples/zipped/mixmas/mixmas_files/0/0_0.jpeg',
   );
   expect(sziTileSource.getTileUrl(10, 55, 33)).toEqual(
-    'http://localhost:5173/examples/compressed/mixmas/mixmas_files/10/55_33.jpeg',
+    'http://localhost:5173/examples/zipped/mixmas/mixmas_files/10/55_33.jpeg',
   );
 });
