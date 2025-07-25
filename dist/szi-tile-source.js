b8bd11e4ceb2208b366efede989970346a2da8ae
Richard Tunnicliffe <rdtunnicliffe@gmail.com>
2025-07-25 17:32:53 +0100

    Brought the dist files up to date
    Just starting to miss having some sort of CI system...

--- dist/szi-tile-source.js
+++ dist/szi-tile-source.js
@@ -1,62 +1,69 @@
-class m {
+class h {
   /**
    * Create the remote file by fetching its size using a head request
    *
-   * @param url url of the file that we eventually want to read
+   * @param {string} url url of the file that we eventually want to read
    * @param fetchOptions options to apply to all fetches,
-   * @param fetchOptions.mode cors mode to use
-   * @param fetchOptions.credentials whether to send credentials
-   * @param fetchOptions.headers additional headers to add to all requests
+   * @param {string} fetchOptions.mode cors mode to use
+   * @param {string} fetchOptions.credentials whether to send credentials
+   * @param {Object} fetchOptions.headers additional headers to add to all requests
    * @returns {Promise<RemoteFile>}
    */
   static create = async (t, e) => {
-    const r = await this.fetchContentLength(t, e);
-    return new m(t, r, e);
+    const n = await this.fetchFileSize(t, e);
+    return new h(t, n, e);
   };
-  static fetchContentLength = async (t, e) => {
-    try {
-      const r = await fetch(t, {
-        method: "HEAD",
-        headers: e.headers,
-        mode: e.mode,
-        credentials: e.credentials
-      });
-      if (!r.ok)
-        throw new Error(`HTTP error! Status: ${r.status}`);
-      const i = r.headers.get("content-length");
-      if (!i)
-        throw new Error("Couldn't get content length from headers");
-      return parseInt(i, 10);
-    } catch (r) {
-      throw console.error("Error getting file size:", r), r;
-    }
+  /**
+   * Attempt to fetch the size of the remote file by doing a HEAD request and reading
+   * the content-length header
+   *
+   * @param {string} url
+   * @param fetchOptions options to apply to all fetches,
+   * @param {string} fetchOptions.mode cors mode to use
+   * @param {string} fetchOptions.credentials whether to send credentials
+   * @param {Object} fetchOptions.headers additional headers to add to all requests
+   * @returns {Promise<number>}
+   */
+  static fetchFileSize = async (t, e) => {
+    const n = await fetch(t, {
+      method: "HEAD",
+      headers: e.headers,
+      mode: e.mode,
+      credentials: e.credentials
+    });
+    if (!n.ok)
+      throw new Error(`Could not fetch size of ${t}, response status for HEAD request: ${n.status}`);
+    const i = n.headers.get("content-length");
+    if (!i)
+      throw new Error(`Could not fetch size of ${t}, no content-length in response headers`);
+    return parseInt(i, 10);
   };
-  constructor(t, e, r) {
-    this.url = t, this.size = e, this.fetchOptions = r;
+  constructor(t, e, n) {
+    this.url = t, this.size = e, this.fetchOptions = n;
   }
   /**
    * Fetch the range of bytes specified. Note that end is *exclusive*, though the header
    * expects *inclusive* values. This removes the need to continually subtract 1 from
    * the more usual end-exclusive values used elsewhere.
    *
-   * @param start inclusive start of range to fetch
-   * @param end exclusive start of range to fetch
-   * @param abortSignal AbortController signal, optionally specify this if you might want to
+   * @param {number} start inclusive start of range to fetch
+   * @param {number} end exclusive start of range to fetch
+   * @param {AbortSignal }abortSignal AbortController signal, optionally specify this if you might want to
    *        abort the request
    * @throws Error if the start or end lie outside the file, or if start > end. Also throws
    *         an error if the request fails with anything other than a status between 200 and
    *         299.
    */
-  fetchRange = async (t, e, r) => {
+  fetchRange = async (t, e, n) => {
     if (t < 0 || t > this.size)
       throw new Error(`Start of fetch range (${t}) out of bounds (0 - ${this.size})!`);
     if (e < 0 || e > this.size)
       throw new Error(`Start of fetch range (${t}) out of bounds (0 - ${this.size})!`);
     if (t > e)
       throw new Error(`Start of fetch range (${t}) greater than end (${e})!`);
-    const i = `bytes=${t}-${e - 1}`, n = this.fetchOptions.headers ? { ...this.fetchOptions.headers, Range: i } : { Range: i }, a = await fetch(this.url, {
-      headers: n,
-      signal: r,
+    const i = `bytes=${t}-${e - 1}`, r = this.fetchOptions.headers ? { ...this.fetchOptions.headers, Range: i } : { Range: i }, a = await fetch(this.url, {
+      headers: r,
+      signal: n,
       mode: this.fetchOptions.mode,
       credentials: this.fetchOptions.credentials
     });
@@ -102,161 +109,177 @@ class u {
   readUint8Array(t) {
     const e = this.pos + t;
     this.checkBounds(e);
-    const r = new Uint8Array(this.buffer, this.pos, t);
-    return this.pos = e, r;
+    const n = new Uint8Array(this.buffer, this.pos, t);
+    return this.pos = e, n;
   }
   skip(t) {
     const e = this.pos + t;
     this.checkBounds(e), this.pos = e;
   }
 }
-const l = 4294967295, w = 65535, S = w, z = 22, U = 20, O = 1, b = 101010256, D = 117853008, L = 101075792, R = 33639248, A = 67324752;
-function I(o, t, e) {
+const l = 4294967295, m = 65535, S = m, z = 22, U = 20, D = 1, b = 101010256, O = 117853008, L = 101075792, R = 33639248, A = 67324752;
+function $(o, t, e) {
   if (e.length > o.length)
     return -1;
   t = Math.min(t, o.length - e.length);
-  for (let r = t; r > -1; r--) {
+  for (let n = t; n > -1; n--) {
     let i = !0;
-    for (let n = 0; n < e.length && i; n++)
-      o.at(r + n) !== e.at(n) && (i = !1);
+    for (let r = 0; r < e.length && i; r++)
+      o.at(n + r) !== e.at(r) && (i = !1);
     if (i)
-      return r;
+      return n;
   }
   return -1;
 }
-function $(o) {
+function I(o) {
   const t = new Uint8Array(4);
   return new DataView(t.buffer).setUint32(0, b, !0), t;
 }
 function k(o) {
-  const t = new Uint8Array(o), e = $();
-  let r = o.byteLength - z;
-  for (; r >= 0; ) {
-    const i = I(t, r, e);
+  const t = new Uint8Array(o), e = I();
+  let n = o.byteLength - z;
+  for (; n >= 0; ) {
+    const i = $(t, n, e);
     if (i === -1)
       throw new Error("Invalid SZI file, no valid End Of Central Directory Record found");
-    const n = new u(o);
-    if (n.skip(i), n.readUint32() !== b)
+    const r = new u(o);
+    if (r.skip(i), r.readUint32() !== b)
       throw new Error("Programming Error: End Of Central Directory Record has unexpected magic number");
-    n.readUint16(), n.readUint16(), n.readUint16();
-    const s = n.readUint16(), c = n.readUint32(), d = n.readUint32(), f = n.readUint16();
-    if (n.pos + f === o.byteLength)
-      return f > 0 && n.readUtf8String(f), { totalEntries: s, centralDirectorySize: c, centralDirectoryOffset: d, startOfEocdInBuffer: i };
-    r = i - 1;
+    r.readUint16(), r.readUint16(), r.readUint16();
+    const s = r.readUint16(), c = r.readUint32(), d = r.readUint32(), f = r.readUint16();
+    if (r.pos + f === o.byteLength)
+      return f > 0 && r.readUtf8String(f), { totalEntries: s, centralDirectorySize: c, centralDirectoryOffset: d, startOfEocdInBuffer: i };
+    n = i - 1;
   }
-  if (r < 0)
+  if (n < 0)
     throw new Error("Invalid SZI file, no End Of Central Directory Record found");
 }
 function v(o, t) {
   const e = new u(o);
-  if (e.skip(t), e.readUint32() !== D)
+  if (e.skip(t), e.readUint32() !== O)
     throw new Error("Invalid SZI file: Zip64 End Of Central Directory Locator has unexpected magic number");
   e.readUint32();
   const i = e.readUint64();
   return e.readUint32(), { zip64EocdOffset: i };
 }
-function C(o, t) {
+function x(o, t) {
   const e = new u(o);
   if (e.skip(t), e.readUint32() !== L)
     throw new Error("Invalid SZI file: Zip64 End Of Central Directory Record has unexpected magic number");
   const i = e.readUint64() + 12;
   e.readUint16(), e.readUint16(), e.readUint32(), e.readUint32(), e.readUint64();
-  const n = e.readUint64(), a = e.readUint64(), s = e.readUint64(), c = i - e.pos - t;
-  return e.skip(c), { totalEntries: n, centralDirectorySize: a, centralDirectoryOffset: s };
+  const r = e.readUint64(), a = e.readUint64(), s = e.readUint64(), c = i - e.pos - t;
+  return e.skip(c), { totalEntries: r, centralDirectorySize: a, centralDirectoryOffset: s };
 }
-function x(o, t, e) {
-  let { compressedSize: r, uncompressedSize: i, diskNumberStart: n, relativeOffsetOfLocalHeader: a } = e;
+function C(o, t, e) {
+  let { compressedSize: n, uncompressedSize: i, diskNumberStart: r, relativeOffsetOfLocalHeader: a } = e;
   const s = o.pos;
   for (; o.pos - s < t; ) {
     const c = o.readUint16(), d = o.readUint16();
-    c === O ? (i === l && (i = o.readUint64()), r === l && (r = o.readUint64()), a === l && (a = o.readUint64()), n === w && (n = o.readUint32())) : o.skip(d);
+    c === D ? (i === l && (i = o.readUint64()), n === l && (n = o.readUint64()), a === l && (a = o.readUint64()), r === m && (r = o.readUint32())) : o.skip(d);
   }
   return {
-    compressedSize: r,
+    compressedSize: n,
     uncompressedSize: i,
-    diskNumberStart: n,
+    diskNumberStart: r,
     relativeOffsetOfLocalHeader: a
   };
 }
 function N(o, t) {
-  const e = new u(o), r = [];
+  const e = new u(o), n = [];
   for (let i = 0; i < t; i++) {
     if (e.readUint32() !== R)
       throw new Error(`Invalid SZI file: Central Directory Header ${i} has unexpected magic number`);
     e.readUint16(), e.readUint16(), e.readUint16(), e.readUint16(), e.readUint16(), e.readUint16(), e.readUint32();
     const a = e.readUint32(), s = e.readUint32(), c = e.readUint16(), d = e.readUint16(), f = e.readUint16(), E = e.readUint16();
     e.readUint16(), e.readUint32();
-    const y = e.readUint32(), g = e.readUtf8String(c);
+    const y = e.readUint32(), p = e.readUtf8String(c);
     if (a !== s)
       throw new Error(
-        `Invalid SZI file: compressedSize: ${a} and uncompressedSize: ${s} don't match for ${g}!`
+        `Invalid SZI file: compressedSize: ${a} and uncompressedSize: ${s} don't match for ${p}!`
       );
-    const h = x(e, d, {
+    const g = C(e, d, {
       compressedSize: a,
       uncompressedSize: s,
       diskNumberStart: E,
       relativeOffsetOfLocalHeader: y
     });
-    e.readUtf8String(f), r.push({
-      compressedSize: h.compressedSize,
-      uncompressedSize: h.uncompressedSize,
-      relativeOffsetOfLocalHeader: h.relativeOffsetOfLocalHeader,
-      filename: g
+    e.readUtf8String(f), n.push({
+      uncompressedSize: g.uncompressedSize,
+      relativeOffsetOfLocalHeader: g.relativeOffsetOfLocalHeader,
+      filename: p
     });
   }
-  return r;
+  return n;
 }
 async function B(o) {
-  const t = Math.max(0, o.size - (U + z + S)), e = await o.fetchRange(t, o.size), { totalEntries: r, centralDirectoryOffset: i, centralDirectorySize: n, startOfEocdInBuffer: a } = k(e);
-  if (r === w || i === l || n === l) {
+  const t = Math.max(0, o.size - (U + z + S)), e = await o.fetchRange(t, o.size), { totalEntries: n, centralDirectoryOffset: i, centralDirectorySize: r, startOfEocdInBuffer: a } = k(e);
+  if (n === m || i === l || r === l) {
     const c = a - U, d = v(e, c), f = await o.fetchRange(
       d.zip64EocdOffset,
       t + c
     );
-    return C(f, 0);
+    return x(f, 0);
   } else
-    return { totalEntries: r, centralDirectoryOffset: i, centralDirectorySize: n };
+    return { totalEntries: n, centralDirectoryOffset: i, centralDirectorySize: r };
 }
-function H(o, t) {
-  const e = /* @__PURE__ */ new Map(), r = o.toSorted(
-    (n, a) => a.relativeOffsetOfLocalHeader - n.relativeOffsetOfLocalHeader
+function F(o, t) {
+  const e = /* @__PURE__ */ new Map(), n = o.toSorted(
+    (r, a) => a.relativeOffsetOfLocalHeader - r.relativeOffsetOfLocalHeader
   );
   let i = t;
-  for (const n of r) {
-    const a = n.relativeOffsetOfLocalHeader;
-    e.set(n.filename, {
+  for (const r of n) {
+    const a = r.relativeOffsetOfLocalHeader;
+    e.set(r.filename, {
       start: a,
       maxEnd: i,
-      bodyLength: n.uncompressedSize
+      bodyLength: r.uncompressedSize
     }), i = a;
   }
   return e;
 }
-async function F(o) {
-  const { totalEntries: t, centralDirectoryOffset: e, centralDirectorySize: r } = await B(o), i = await o.fetchRange(e, e + r), n = N(i, t);
-  return H(n, e);
+async function H(o) {
+  const { totalEntries: t, centralDirectoryOffset: e, centralDirectorySize: n } = await B(o), i = await o.fetchRange(e, e + n), r = N(i, t);
+  return F(r, e);
 }
-class p {
+class w {
+  /**
+   * Asynchronously create an instance of a reader for the supplied SZI remote file
+   * @param {RemoteFile} sziFile
+   * @returns {Promise<SziFileReader>}
+   */
   static create = async (t) => {
-    const e = await F(t);
-    return new p(t, e);
+    const e = await H(t);
+    return new w(t, e);
   };
   constructor(t, e) {
     this.sziFile = t, this.contents = e;
   }
+  /**
+   * Read the body of the filename contained in the SZI file
+   *
+   * @param {string} filename filename whose body you want to read
+   * @param {AbortSignal} abortSignal AbortController.signal for cancelling the request
+   * @returns {Promise<Uint8Array>} The body of the file specified
+   */
   fetchFileBody = async (t, e) => {
-    const r = this.contents.get(t);
-    if (!r)
+    const n = this.contents.get(t);
+    if (!n)
       throw new Error(`${t} is not present inside this .szi file`);
-    const i = await this.sziFile.fetchRange(r.start, r.maxEnd, e), n = new u(i, 0);
-    if (n.readUint32() !== A)
+    const i = await this.sziFile.fetchRange(n.start, n.maxEnd, e), r = new u(i, 0);
+    if (r.readUint32() !== A)
       throw new Error(`Invalid SZI file: Local Header for ${t} has unexpected magic number`);
-    n.readUint16(), n.readUint16(), n.readUint16(), n.readUint16(), n.readUint16(), n.readUint32(), n.readUint32(), n.readUint32();
-    const s = n.readUint16(), c = n.readUint16(), d = n.readUtf8String(s);
+    r.readUint16(), r.readUint16(), r.readUint16(), r.readUint16(), r.readUint16(), r.readUint32(), r.readUint32(), r.readUint32();
+    const s = r.readUint16(), c = r.readUint16(), d = r.readUtf8String(s);
     if (d !== t)
       throw new Error(`Trying to read ${t} but actually got ${d}`);
-    return n.skip(c), n.readUint8Array(r.bodyLength);
+    return r.skip(c), r.readUint8Array(n.bodyLength);
   };
+  /**
+   * Find the filename of the .dzi config file inside the contents
+   *
+   * @returns {string}
+   */
   dziFilename = () => {
     let t = "";
     for (const e of this.contents.keys())
@@ -269,6 +292,13 @@ class p {
       throw new Error("No dzi file found in .szi!");
     return t;
   };
+  /**
+   * Find the top level tiles directory. This should be of the form
+   * <name>/<name>_files, and contain subdirectories containing tiles
+   * for each zoom level
+   *
+   * @returns {string}
+   */
   tilesDirectory = () => {
     const e = this.dziFilename().split("/")[0];
     return `${e}/${e}_files/`;
@@ -279,7 +309,7 @@ const Z = (o) => {
     /**
      * Create an SZI tile source for use with OpenSeadragon.
      *
-     * @param url location of the SZI file we want to read
+     * @param {string} url location of the SZI file we want to read
      * @param fetchOptions options to use when making HTTP requests to fetch parts of the file
      * @param fetchOptions.mode cors mode to use. Note that "no-cors" is not accepted, as it breaks Range requests.
      *        (See: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#making_cross-origin_requests)
@@ -288,18 +318,18 @@ const Z = (o) => {
      * @param fetchOptions.headers additional HTTP headers to send with each request
      * @returns {Promise<SziTileSource>}
      */
-    static createSziTileSource = async (r, i = {}) => {
+    static createSziTileSource = async (n, i = {}) => {
       if (i && i.mode === "no-cors")
         throw new Error("'no-cors' mode is not supported, as Range headers don't work with it");
-      const n = await m.create(r, i), a = await p.create(n), s = await this.readOptionsFromDziXml(a);
+      const r = await h.create(n, i), a = await w.create(r), s = await this.readOptionsFromDziXml(a);
       return new t(a, s);
     };
-    static async readOptionsFromDziXml(r) {
-      const i = r.dziFilename(), n = await r.fetchFileBody(i), a = new TextDecoder().decode(n), s = o.parseXml(a);
+    static async readOptionsFromDziXml(n) {
+      const i = n.dziFilename(), r = await n.fetchFileBody(i), a = new TextDecoder().decode(r), s = o.parseXml(a);
       return o.DziTileSource.prototype.configure(s, i, "");
     }
-    constructor(r, i) {
-      super(i), this.remoteSziReader = r;
+    constructor(n, i) {
+      super(i), this.remoteSziReader = n;
     }
     /**
      * Download tile data. This is a cut down implementation of the XML-specific path of TileSource.Download
@@ -313,23 +343,23 @@ const Z = (o) => {
      * @param {*} [context.userData] - Empty object to attach your own data and helper variables to.
      * @param {Function} [context.finish] - Should be called unless abort() was executed, e.g. on all occasions,
      */
-    downloadTileStart = (r) => {
+    downloadTileStart = (n) => {
       const i = new Image();
       i.onload = function() {
-        n(), r.finish(i, r.userData.request, null);
+        r(), n.finish(i, n.userData.request, null);
       }, i.onabort = i.onerror = function() {
-        n(), r.finish(null, r.userData.request, "Image load aborted.");
+        r(), n.finish(null, n.userData.request, "Image load aborted.");
       };
-      const n = () => {
+      const r = () => {
         i.onload = i.onerror = i.onabort = null;
       };
-      r.userData.image = i, r.userData.abortController = new AbortController(), this.remoteSziReader.fetchFileBody(r.src, r.userData.abortController.signal).then(
+      n.userData.image = i, n.userData.abortController = new AbortController(), this.remoteSziReader.fetchFileBody(n.src, n.userData.abortController.signal).then(
         (a) => {
           const s = new Blob([a]);
-          s.size === 0 ? (n(), r.finish(null, null, "Empty image!")) : i.src = (window.URL || window.webkitURL).createObjectURL(s);
+          s.size === 0 ? (r(), n.finish(null, null, "Empty image!")) : i.src = (window.URL || window.webkitURL).createObjectURL(s);
         },
         (a) => {
-          n(), r.finish(null, null, "Download failed: " + a.message);
+          r(), n.finish(null, null, "Download failed: " + a.message);
         }
       );
     };
@@ -339,11 +369,11 @@ const Z = (o) => {
      * @param {ImageJob} context job, the same object as with downloadTileStart(..)
      * @param {*} [context.userData] - Empty object to attach (and mainly read) your own data.
      */
-    downloadTileAbort = (r) => {
-      const i = r.userData.abortController;
+    downloadTileAbort = (n) => {
+      const i = n.userData.abortController;
       i && i.abort();
-      const n = r.userData.image;
-      n && (n.onload = n.onerror = n.onabort = null);
+      const r = n.userData.image;
+      r && (r.onload = r.onerror = r.onabort = null);
     };
   }
   o.SziTileSource = t;
