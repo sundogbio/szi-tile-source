2ca7ac3e04499fc1d50b83cd8151da03ea98b7f8
Tom Armitage <tom@infovore.org>
2025-07-29 10:24:34 +0100

    Embed version number on demo page.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import { dirname, resolve } from 'node:path'
 
-export default defineConfig(({command}) => ({
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
+console.log(packageJson.version);
+
+export default defineConfig(({ command }) => ({
   // Otherwise the public directory ends up in dist. There is probably a better way
   // of doing this, but until I reorganise the folder structure, this is fine for now!
   publicDir: command === 'build' ? false : 'public',
-  build : {
+  define: {
+    __VERSION__: JSON.stringify(packageJson.version),
+  },
+  build: {
     lib: {
       entry: resolve(__dirname, 'src/main.js'),
       name: 'SziTileSource',
       // the proper extensions will be added
       fileName: 'szi-tile-source',
     },
-  }
-}))
+  },
+}));
