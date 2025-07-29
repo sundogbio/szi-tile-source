11e70a2ed825cfd575c5340efc87f6f974846b0e
Tom Armitage <tom@infovore.org>
2025-07-29 11:32:52 +0100

    Build demo site for Github pages (#1)
    Add another vite config and corresponding package command to build the demo site to `dist`, ready to post to Github pages. Tidy up formatting of demo site. Add version number to demo site automatically via Vite config. Add workflow file to build site and publish to pages on commits to `main`.

---
+++ pages.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { readFileSync } from 'fs';
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+export default defineConfig(({ command }) => ({
+  define: {
+    __VERSION__: JSON.stringify(packageJson.version),
+  },
+}));
